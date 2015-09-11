ab3063869d50781d4cccb1713a4ffaf5ab5f2029
Stephen Mathieson <me@stephenmathieson.com>
2015-09-11 14:21:08 -0400

    test: refactor/dry out cases

--- test.c
+++ test.c
@@ -5,38 +5,19 @@
 
 #include "asprintf.h"
 
+#define test(expected_size, fmt, ...) ({          \
+  char *str = NULL;                               \
+  int size = asprintf(&str, fmt, __VA_ARGS__);    \
+  assert(str);                                    \
+  assert(expected_size == size);                  \
+  free(str);                                      \
+})
+
+
 int
 main (void) {
-  {
-    char *str = NULL;
-    char *fmt = "this is a %s";
-    int size = asprintf(&str, fmt, "string");
-
-    assert(str);
-    assert(16 == size);
-    free(str);
-  }
-
-  {
-    char *str = NULL;
-    char *fmt = "%d + %d = %d";
-    int size = asprintf(&str, fmt, 1, 1, 2);
-
-    assert(str);
-    assert(9 == size);
-    free(str);
-  }
-
-  {
-    char *str = NULL;
-    char *fmt = "bradley likes %s, %s, %s, and %s";
-    int size = asprintf(&str, fmt,
-        "kinkajous", "bananas", "monkeys", "the beach");
-
-    assert(str);
-    assert(56 == size);
-    free(str);
-  }
-
+  test(16, "this is a %s", "string");
+  test(9, "%d + %d = %d", 1, 1, 2);
+  test(56, "bradley likes %s, %s, %s, and %s", "kinkajous", "bananas", "monkeys", "the beach");
   return 0;
 }
